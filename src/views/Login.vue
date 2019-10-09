9a2e9c69921a95d103da1733354f7aea775f7d40
Maximiliano Orellana <maximiliano.orellana.a@usach.cl>
2019-10-09 16:33:07 -0300

    Added login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,68 @@
 <template>
-  <div class="container">
-    <h1>Login page</h1>
-  </div>
+	<div class="container">
+		<b-row>
+			<b-col></b-col>
+			<b-col>
+			<b-card
+				header="Login"
+				header-tag="header"
+			>
+				<b-form @submit="onSubmit" v-if="show">
+					<b-form-group
+						id="input-group-1"
+						label="Email address:"
+						label-for="input-1"
+					>
+					<b-form-input
+						id="input-1"
+						v-model="form.email"
+						type="email"
+						required
+						placeholder="Enter email"	
+					>
+					</b-form-input>
+					</b-form-group>
+					<b-form-group
+						id="input-group-2"
+						label="Password:"
+						label-for="input-2"
+					>
+					<b-form-input
+						id="input-2"
+						type="password"
+						required
+						placeholder="Enter password"	
+					>
+					</b-form-input>
+					</b-form-group>
+					<b-button type="submit" variant="primary">Login</b-button>
+				</b-form>
+			</b-card>
+			</b-col>
+			<b-col></b-col>
+		</b-row>
+	</div>
 </template>
+
+<script>
+	export default {
+		data() {
+			return {
+				form: {
+					email: '',
+					password: ''
+				},
+				show: true
+			}
+		},
+		methods: {
+			onSubmit(evt) {
+				evt.preventDefault()
+				alert(JSON.stringify(this.form))
+			},
+		}
+	}
+</script>
+
+<style scoped>
+</style>
