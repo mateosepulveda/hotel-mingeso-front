c03edb0dce1f56fd34699de05190cd505f25e922
matias <matias.pizarro@usach.cl>
2019-10-14 18:00:14 -0300

    fix

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+export const rest_ip = "http://localhost:8080/";
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: () => import('./views/Login.vue')
+    },
+    {
+      path: '/createRoom',
+      name: 'create-room',
+      component: () => import('./views/createRoom.vue')
     },
     {
       path: '/rack',
